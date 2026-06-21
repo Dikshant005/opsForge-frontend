be2156434b71a3a9ab658a3156b9f2bdcf0bd251
Dikshant <dikshantahalawat2005@gmail.com>
2026-06-21 19:51:13 +0530

    add async thunk to fetch current user and handle auth states

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -1,12 +1,29 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axiosClient from '../../api/axiosClient';
 
 const tokenFromStorage = localStorage.getItem('token');
+
+
+// Creating Thunk
+export const fetchCurrentUser = createAsyncThunk(
+  'auth/fetchCurrentUser',
+  async (_, thunkAPI) => {
+    try{
+        const response = await axiosClient.get('/api/users/me')
+        return response.data
+    }
+    catch(error){
+        return thunkAPI.rejectWithValue(error.response.data)
+    }
+  },
+)
 
 const initialState = {
   // Define your initial state properties here
   user: null,
   token: tokenFromStorage, // hydrate token from localStorage if it exists
   isAuthenticated: !!tokenFromStorage, // Set to true if token exists, false otherwise
+  status: 'idle', // 'idle' | 'loading' | 'succeeded' | 'failed'
 };
 
 const authSlice = createSlice({
@@ -30,6 +47,21 @@ const authSlice = createSlice({
       localStorage.removeItem('token');
     },
   },
+  extraReducers: (builder) => {
+    builder.addCase(fetchCurrentUser.pending, (state) => {
+      state.status = 'loading';
+    });
+    builder.addCase(fetchCurrentUser.fulfilled, (state, action) => {
+      state.status = 'succeeded';
+      state.user = action.payload;
+    });
+    builder.addCase(fetchCurrentUser.rejected, (state) => {
+      state.status = 'failed';
+      state.user = null;
+      state.isAuthenticated = false;
+      localStorage.removeItem('token');
+    });
+  }
 });
 
 export const { setCredentials, logout } = authSlice.actions;
